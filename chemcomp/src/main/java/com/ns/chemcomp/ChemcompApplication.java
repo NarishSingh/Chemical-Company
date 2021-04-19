e609ba3da2a9f15fadbb25d91d48d302333e7bd6
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-04-19 15:22:36 -0400

    Fix to spring/maven issue

---
+++ chemcomp/src/main/java/com/ns/chemcomp/ChemcompApplication.java
@@ -0,0 +1,12 @@
+package com.ns.chemcomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemcompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChemcompApplication.class, args);
+    }
+}
---
+++ chemcomp/src/test/java/com/ns/chemcomp/ChemcompApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ns.chemcomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ChemcompApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
