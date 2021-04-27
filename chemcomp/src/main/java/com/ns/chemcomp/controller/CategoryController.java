37c2b435b2ce13d77579e45655e264e2c850c972
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-04-27 15:12:55 -0400

    Product list by category view started

--- chemcomp/src/main/java/com/ns/chemcomp/controller/CategoryController.java
+++ chemcomp/src/main/java/com/ns/chemcomp/controller/CategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -18,18 +19,41 @@ public class CategoryController {
     @Autowired
     CategoryDao cDao;
 
+    /**
+     * GET - category list page view
+     *
+     * @param model {Model} holds list of categories, and array of product counts
+     * @return {String} load view
+     */
     @GetMapping({"/categories"})
     public String displayCategoriesPage(Model model) {
         List<Category> categories = cDao.readAllCategories();
 
-        //get product counts
+        //get product counts for badge pill
         int[] productCounts = categories.stream()
-                .mapToInt(c -> pDao.readProductsByCategory(c).size())
+                .mapToInt(c -> pDao.readProductsByCategory(c.getCategoryId()).size())
                 .toArray();
 
         model.addAttribute("categories", categories);
         model.addAttribute("productCounts", productCounts);
 
         return "categories";
+    }
+
+    /**
+     * GET - list of products for selected category view
+     *
+     * @param model   {Model} holds list of products
+     * @param request {HttpServletRequest} used to pull id from request
+     * @return {String} load view
+     */
+    @GetMapping({"/viewCategory"})
+    public String displayViewCategoryPage(Model model, HttpServletRequest request) {
+        int categoryId = Integer.parseInt(request.getParameter("id"));
+
+        model.addAttribute("category", cDao.readCategoryById(categoryId));
+        model.addAttribute("products", pDao.readProductsByCategory(categoryId));
+
+        return "viewCategory";
     }
 }
