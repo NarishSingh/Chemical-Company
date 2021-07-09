397fa1f09257e2c4b025edf465d392d7c6ae4eb3
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-07-09 16:08:00 -0400

    Add user as admin controller method drafted, added Spring security tags

--- chemcomp/src/main/java/com/ns/chemcomp/controller/AdminController.java
+++ chemcomp/src/main/java/com/ns/chemcomp/controller/AdminController.java
@@ -2,13 +2,19 @@ package com.ns.chemcomp.controller;
 
 import com.ns.chemcomp.dao.RoleDao;
 import com.ns.chemcomp.dao.UserDao;
+import com.ns.chemcomp.dto.Role;
 import com.ns.chemcomp.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,6 +24,8 @@ public class AdminController {
     RoleDao rDao;
     @Autowired
     UserDao uDao;
+    @Autowired
+    PasswordEncoder encoder;
     Set<ConstraintViolation<User>> violations = new HashSet<>();
 
     /**
@@ -33,5 +41,43 @@ public class AdminController {
         model.addAttribute("errors", violations);
 
         return "admin";
+    }
+
+    /**
+     * POST - create a new user account as an Admin - created account will be automatically enabled
+     *
+     * @param request marshals form data
+     * @return reload admin view
+     */
+    @PostMapping({"/addUser"})
+    public String addUserAsAdmin(HttpServletRequest request) {
+        User user = new User();
+        String usernameString = request.getParameter("username");
+        String passwordString = request.getParameter("password");
+
+        if (usernameString.isBlank() || passwordString.isBlank()) {
+            return "admin";
+        } else {
+            user.setUsername(usernameString);
+            user.setPassword(encoder.encode(passwordString));
+            user.setEnabled(true);
+            user.setName(request.getParameter("name"));
+            user.setPhone(request.getParameter("phone"));
+            user.setEmail(request.getParameter("email"));
+            user.setAddress(request.getParameter("address"));
+
+            Set<Role> userRoles = new HashSet<>();
+            userRoles.add(rDao.readRoleById(Integer.parseInt(request.getParameter("roleId"))));
+            user.setRoles(userRoles);
+        }
+
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violations = validate.validate(user);
+
+        if (violations.isEmpty()) {
+            uDao.createUser(user);
+        }
+
+        return "redirect:/admin";
     }
 }
